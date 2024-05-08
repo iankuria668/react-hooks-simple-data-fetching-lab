1049c037d7e6923ef3ceb239354c862755e4bdd5
iankuria668 <iankuria668@gmail.com>
2024-05-08 08:23:29 +0300

    App Components works correctly

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import { useState, useEffect } from "react";
+
+function App() {
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((response) => response.json())
+            .then((data) => {
+                setData(data);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error Fetching dog images',error);
+                setIsLoading(false);
+            });
+    }, []);
+
+    return(
+        <div>
+            {isLoading ? <p>Loading...</p> : (<img src={data.message} alt="A Random Dog" />)}
+        </div>
+    );
+}
+
+export default App;
